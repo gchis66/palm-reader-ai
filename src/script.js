153a76a63e150771f41d04fe3ad22633a918f39f
gchis66 <gchis66@gmail.com>
2024-11-22 14:46:27 -0500

    testing out new camera setup

--- src/script.js
+++ src/script.js
@@ -14,218 +14,222 @@ document.addEventListener("DOMContentLoaded", function () {
       crossFade: true,
     },
   });
-});
 
-// Initialize Stripe (replace 'your_stripe_public_key' with your actual Stripe public key)
-const stripe = Stripe(
-  "pk_live_51LFOR1EGWtwpkkhteYPKdviYKkkFCjoE0DC4ZweTNEUVhsW5gsmIUjaLoC3lK6P2UD6uoaId5fIAc7aWXFUEVn2B00OjikTqmE"
-);
-// Define a custom style for the Stripe Elements
-const style = {
-  base: {
-    color: "#000",
-    fontWeight: 500,
-    fontFamily: "Source Code Pro, Consolas, Menlo, monospace",
-    fontSize: "16px",
-    fontSmoothing: "antialiased",
-    "::placeholder": {
-      color: "#aab7c4",
+  const stripe = Stripe(
+    "pk_live_51LFOR1EGWtwpkkhteYPKdviYKkkFCjoE0DC4ZweTNEUVhsW5gsmIUjaLoC3lK6P2UD6uoaId5fIAc7aWXFUEVn2B00OjikTqmE"
+  );
+
+  const style = {
+    base: {
+      color: "#000",
+      fontWeight: 500,
+      fontFamily: "Source Code Pro, Consolas, Menlo, monospace",
+      fontSize: "16px",
+      fontSmoothing: "antialiased",
+      "::placeholder": {
+        color: "#aab7c4",
+      },
     },
-  },
-  invalid: {
-    color: "#fa755a",
-    iconColor: "#fa755a",
-  },
-};
+    invalid: {
+      color: "#fa755a",
+      iconColor: "#fa755a",
+    },
+  };
 
-// Initialize Stripe Elements
-const elements = stripe.elements();
-const cardElement = elements.create("card", { style: style });
-cardElement.mount("#card-element");
+  const elements = stripe.elements();
+  const cardElement = elements.create("card", { style: style });
+  cardElement.mount("#card-element");
 
-document.getElementById("uploadButton").addEventListener("click", function () {
-  const canvas = document.getElementById("canvas");
-  const imageInput = document.getElementById("imageInput");
-  const formData = new FormData();
-
-  if (imageInput.files.length > 0) {
-    const imageData = imageInput.files[0];
-    formData.append("palmImage", imageData);
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
-  } else if (canvas.toDataURL() !== "data:,") {
-    const imageData = dataURItoBlob(canvas.toDataURL());
-    formData.append("palmImage", imageData, "palmImage.png");
-  } else {
-    alert("Please select an image to upload.");
-    return;
-  }
-
-  const modal = document.getElementById("modal");
-  const modalText = document.getElementById("modal-text");
-  const modalLoading = document.getElementById("modal-loading");
-  const closeSpan = document.getElementsByClassName("close")[0];
-
-  modal.style.display = "block";
-  modalLoading.style.display = "block";
-  modalText.textContent = "Please wait while your palm is being read...";
-
-  fetch("https://palm-reader-app.onrender.com/api/upload", {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        if (response.status === 413) {
-          throw new Error(
-            "The image is too large. Please upload an image smaller than 20MB."
-          );
-        }
-        if (response.status === 429) {
-          throw new Error("Too many requests. Please try again later.");
-        }
-        throw new Error("An error occurred while processing your request.");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data.message);
-      const [previewContent, fullContent] = splitContent(data.message);
-      modalText.innerHTML = previewContent;
-      document.getElementById("payment-info-container").style.display = "block";
-      const paymentButton = document.createElement("button");
-      paymentButton.classList.add("paymentbtn");
-      paymentButton.textContent = "Unlock Full Reading for $4.99";
-      paymentButton.onclick = () =>
-        openStripeCheckout(previewContent + fullContent);
-      modalText.appendChild(paymentButton);
-      modalLoading.style.display = "none";
-      closeSpan.style.pointerEvents = "auto";
-      document.body.style.overflowY = "hidden"; // Disable scrolling
-    })
-    .catch((error) => {
-      modalText.textContent = error.message;
-      modalLoading.style.display = "none";
-      closeSpan.style.pointerEvents = "auto";
-    });
-});
-
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function () {
-  document.getElementById("modal").style.display = "none";
-  document.body.style.overflowY = "auto"; // Enable scrolling
-};
-
-document.getElementById("imageInput").addEventListener("change", function () {
-  const imageInput = document.getElementById("imageInput");
-  const uploadButton = document.getElementById("uploadButton");
+  // Camera functionality
   const cameraStream = document.getElementById("cameraStream");
+  const canvas = document.createElement("canvas");
+  const openCameraButton = document.getElementById("openCameraButton");
   const snapButton = document.getElementById("snapButton");
+  const preview = document.getElementById("preview");
+  const uploadButton = document.getElementById("uploadButton");
+  const imageInput = document.getElementById("imageInput");
 
-  cameraStream.style.display = "none";
-  snapButton.style.display = "none";
+  canvas.style.display = "none";
+  document.body.appendChild(canvas);
 
-  if (imageInput.files.length > 0) {
-    const preview = document.getElementById("preview");
-    preview.innerHTML =
-      '<img src="' + URL.createObjectURL(imageInput.files[0]) + '">';
-    uploadButton.style.display = "block";
-  }
-});
-
-document
-  .getElementById("openCameraButton")
-  .addEventListener("click", function () {
-    const cameraStream = document.getElementById("cameraStream");
-    const canvas = document.getElementById("canvas");
-    const snapButton = document.getElementById("snapButton");
-    const preview = document.getElementById("preview");
-    const uploadButton = document.getElementById("uploadButton");
-    const imageInput = document.getElementById("imageInput");
-
-    imageInput.value = "";
-    uploadButton.style.display = "none";
+  openCameraButton.addEventListener("click", function () {
     preview.innerHTML = "";
+    uploadButton.style.display = "none";
     cameraStream.style.display = "block";
     snapButton.style.display = "inline-block";
+    imageInput.value = "";
 
     if (navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices
-        .getUserMedia({ video: { facingMode: { ideal: "environment" } } })
+        .getUserMedia({
+          video: {
+            facingMode: { ideal: "environment" },
+            width: { ideal: 1280 },
+            height: { ideal: 720 },
+          },
+        })
         .then(function (stream) {
           cameraStream.srcObject = stream;
+          cameraStream.play();
         })
         .catch(function (error) {
-          console.log("Something went wrong opening the camera.");
+          console.error("Camera error:", error);
         });
     }
   });
 
-document.getElementById("snapButton").addEventListener("click", function () {
-  const cameraStream = document.getElementById("cameraStream");
-  const canvas = document.getElementById("canvas");
-  const context = canvas.getContext("2d");
-  const snapButton = document.getElementById("snapButton");
+  snapButton.addEventListener("click", function () {
+    canvas.width = cameraStream.videoWidth;
+    canvas.height = cameraStream.videoHeight;
+    canvas.getContext("2d").drawImage(cameraStream, 0, 0);
+    const imageDataURL = canvas.toDataURL("image/jpeg");
+    preview.innerHTML = `<img src="${imageDataURL}" alt="Captured palm">`;
 
-  context.drawImage(cameraStream, 0, 0, canvas.width, canvas.height);
-  cameraStream.srcObject.getVideoTracks().forEach((track) => track.stop());
+    if (cameraStream.srcObject) {
+      cameraStream.srcObject.getTracks().forEach((track) => track.stop());
+    }
 
-  const dataURL = canvas.toDataURL("image/png");
-  const preview = document.getElementById("preview");
-  preview.innerHTML = '<img src="' + dataURL + '">';
+    cameraStream.style.display = "none";
+    snapButton.style.display = "none";
+    uploadButton.style.display = "block";
+  });
 
-  const uploadButton = document.getElementById("uploadButton");
-  uploadButton.style.display = "block";
+  imageInput.addEventListener("change", function () {
+    const imageInput = document.getElementById("imageInput");
+    const uploadButton = document.getElementById("uploadButton");
+    const cameraStream = document.getElementById("cameraStream");
+    const snapButton = document.getElementById("snapButton");
 
-  cameraStream.style.display = "none";
-  snapButton.style.display = "none";
-});
+    cameraStream.style.display = "none";
+    snapButton.style.display = "none";
 
-function dataURItoBlob(dataURI) {
-  const byteString = atob(dataURI.split(",")[1]);
-  const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
-  const arrayBuffer = new ArrayBuffer(byteString.length);
-  const intArray = new Uint8Array(arrayBuffer);
+    if (imageInput.files.length > 0) {
+      const preview = document.getElementById("preview");
+      preview.innerHTML =
+        '<img src="' + URL.createObjectURL(imageInput.files[0]) + '">';
+      uploadButton.style.display = "block";
+    }
+  });
 
-  for (let i = 0; i < byteString.length; i++) {
-    intArray[i] = byteString.charCodeAt(i);
+  uploadButton.addEventListener("click", function () {
+    const formData = new FormData();
+    let imageData;
+
+    if (imageInput.files.length > 0) {
+      imageData = imageInput.files[0];
+      formData.append("palmImage", imageData);
+      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    } else if (canvas.toDataURL() !== "data:,") {
+      imageData = dataURItoBlob(canvas.toDataURL());
+      formData.append("palmImage", imageData, "palmImage.png");
+    } else {
+      alert("Please select an image to upload.");
+      return;
+    }
+
+    const modal = document.getElementById("modal");
+    const modalText = document.getElementById("modal-text");
+    const modalLoading = document.getElementById("modal-loading");
+    const closeSpan = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+    modalLoading.style.display = "block";
+    modalText.textContent = "Please wait while your palm is being read...";
+
+    fetch("https://palm-reader-app.onrender.com/api/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 413) {
+            throw new Error(
+              "The image is too large. Please upload an image smaller than 20MB."
+            );
+          }
+          if (response.status === 429) {
+            throw new Error("Too many requests. Please try again later.");
+          }
+          throw new Error("An error occurred while processing your request.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.message);
+        const [previewContent, fullContent] = splitContent(data.message);
+        modalText.innerHTML = previewContent;
+        document.getElementById("payment-info-container").style.display =
+          "block";
+        const paymentButton = document.createElement("button");
+        paymentButton.classList.add("paymentbtn");
+        paymentButton.textContent = "Unlock Full Reading for $4.99";
+        paymentButton.onclick = () =>
+          openStripeCheckout(previewContent + fullContent);
+        modalText.appendChild(paymentButton);
+        modalLoading.style.display = "none";
+        closeSpan.style.pointerEvents = "auto";
+        document.body.style.overflowY = "hidden";
+      })
+      .catch((error) => {
+        modalText.textContent = error.message;
+        modalLoading.style.display = "none";
+        closeSpan.style.pointerEvents = "auto";
+      });
+  });
+
+  const closeSpan = document.getElementsByClassName("close")[0];
+  closeSpan.onclick = function () {
+    document.getElementById("modal").style.display = "none";
+    document.body.style.overflowY = "auto";
+  };
+
+  function dataURItoBlob(dataURI) {
+    const byteString = atob(dataURI.split(",")[1]);
+    const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const intArray = new Uint8Array(arrayBuffer);
+
+    for (let i = 0; i < byteString.length; i++) {
+      intArray[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([arrayBuffer], { type: mimeString });
   }
 
-  return new Blob([arrayBuffer], { type: mimeString });
-}
+  function splitContent(fullText) {
+    const splitIndex = fullText.indexOf("<!-- PAYWALL -->");
+    const previewContent = fullText.substring(0, splitIndex);
+    const fullContent = fullText.substring(splitIndex);
+    return [previewContent, fullContent];
+  }
 
-function splitContent(fullText) {
-  const splitIndex = fullText.indexOf("<!-- PAYWALL -->");
-  const previewContent = fullText.substring(0, splitIndex);
-  const fullContent = fullText.substring(splitIndex);
-  return [previewContent, fullContent];
-}
-
-function openStripeCheckout(completeContent) {
-  fetch("https://palm-reader-app.onrender.com/create-payment-intent", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      return stripe.confirmCardPayment(data.clientSecret, {
-        payment_method: {
-          card: cardElement,
-        },
-      });
+  function openStripeCheckout(completeContent) {
+    fetch("https://palm-reader-app.onrender.com/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    .then((result) => {
-      if (result.error) {
-        console.error(result.error.message);
-      } else {
-        if (result.paymentIntent.status === "succeeded") {
-          document.getElementById("card-element").style.display = "none";
-          document.getElementById("modal-text").innerHTML = completeContent;
+      .then((response) => response.json())
+      .then((data) => {
+        return stripe.confirmCardPayment(data.clientSecret, {
+          payment_method: {
+            card: cardElement,
+          },
+        });
+      })
+      .then((result) => {
+        if (result.error) {
+          console.error(result.error.message);
+        } else {
+          if (result.paymentIntent.status === "succeeded") {
+            document.getElementById("card-element").style.display = "none";
+            document.getElementById("modal-text").innerHTML = completeContent;
+          }
         }
-      }
-    })
-    .catch((error) => {
-      console.error("Payment failed:", error);
-    });
-}
+      })
+      .catch((error) => {
+        console.error("Payment failed:", error);
+      });
+  }
+});
